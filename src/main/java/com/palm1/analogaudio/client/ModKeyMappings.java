894d78ca0393075224b3a41c8c39f7d88b10a02b
Palm <christopher.k.izatt@gmail.com>
2026-05-14 01:39:04 -0400

    Fixed server error caused by new keybind.

--- src/main/java/com/palm1/analogaudio/client/ModKeyMappings.java
+++ src/main/java/com/palm1/analogaudio/client/ModKeyMappings.java
@@ -3,13 +3,14 @@ package com.palm1.analogaudio.client;
 import com.mojang.blaze3d.platform.InputConstants;
 import com.palm1.analogaudio.AnalogAudio;
 import net.minecraft.client.KeyMapping;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.neoforged.neoforge.client.settings.KeyConflictContext;
 import org.lwjgl.glfw.GLFW;
 
-@EventBusSubscriber(modid = AnalogAudio.MODID)
+@EventBusSubscriber(modid = AnalogAudio.MODID, value = Dist.CLIENT)
 public class ModKeyMappings {
     public static final KeyMapping SWITCH_INPUT_MODE = new KeyMapping(
             "key.analogaudio.switch_input_mode",
